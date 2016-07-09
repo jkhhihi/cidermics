ab303415fa9c6ee24eefa609db2f7ad93df2e3c4
Hwang <Hwang@Nodo-Voovo>
2016-07-09 19:07:56 +0900

    콘텐츠 관리자 기능 추가

--- routes/admin.js
+++ routes/admin.js
@@ -80,8 +80,18 @@ router.get('/contents', function(req, res, next) {
 });
 
 router.get('/contents/insert', function(req, res, next) {
+	
 	var CP = 1;
-	res.render('admin/contents/insert', { CP : CP });
+	
+	mysql.select('select * from cider.cid_con_cate', function (err, data){
+		if(err){
+			res.redirect('back');
+		}
+		console.log(data);
+		res.render('admin/contents/insert', {cate : data, CP : CP });
+		
+    });
+	
 });
 
 router.get('/contents/files/:page', function(req, res, next){
@@ -215,13 +225,20 @@ router.get('/contents/detail/:no', function(req, res, next) {
 	
 	var CP = 2;
 	var no = req.params.no;
+	var cate = 
 	
-	mysql.select('select * from cider.cid_contents where con_no = '+ no +'', function (err, data){
+	mysql.select('select * from cider.cid_con_cate', function (err, data){
 		if(err){
 			res.redirect('back');
 		}
-		res.render('admin/contents/update', {contents : data, CP : CP});
-    	
+			mysql.select('select * from cider.cid_contents where con_no = '+ no +'', function (err, data2){
+				if(err){
+					res.redirect('back');
+				}
+				res.render('admin/contents/update', {contents : data2, CP : CP, cate : data,});
+				
+			});
+		
     });
 	
 });
