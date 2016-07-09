07d8f06a159329ed6878c58ecff897198faa704d
jkhhihi <jkhhihi@naver.com>
2016-07-09 19:06:51 +0900

    * 주소 url cid 없애고 간략화함.
    main페이지 수정
    
    routes main.js 삭제함.
    
    top,bottom.ejs 수정

--- routes/consulting.js
+++ routes/consulting.js
@@ -2,13 +2,13 @@ var express = require('express');
 var router = express.Router();
 //var mysql = require("./model/mysql");
 
-router.get('/cid_consulting', function(req, res, next) {
+router.get('/consulting', function(req, res, next) {
 
 	res.render('front/cid_consulting/cid_consulting', { });
 
 });
 
-router.get('/cid_consulting_detail', function(req, res, next) {
+router.get('/consulting_detail', function(req, res, next) {
 
 	res.render('front/cid_consulting/cid_consulting_detail', { });
 
