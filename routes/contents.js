8391d784aee9d1f75517e0421ed1b67a646c2fb3
jkhhihi <jkhhihi@naver.com>
2016-07-11 17:28:57 +0900

    *수정사항
    메인 페이지, 콘텐츠 페이지
    
    콘텐츠 화면에 뿌려줌.

--- routes/contents.js
+++ routes/contents.js
@@ -1,16 +1,52 @@
 var express = require('express');
 var router = express.Router();
-//var mysql = require("./model/mysql");
+var mysql = require("./model/mysql");
 
 router.get('/contents', function(req, res, next) {
-
-	res.render('front/cid_contents/cid_contents', { });
+	
+	var row;
+	mysql.select('select con_no, con_photo, con_title from cider.cid_contents where con_category = "1" order by con_no desc limit 0,12', function (err, data){
+		 if (err) throw err;
+		 console.log("data");
+		 console.log(data);
+		 
+		 row = data;
+		 res.render('front/cid_contents/cid_contents', { contents : row});
+	});
 
 });
 
-router.get('/contents_detail', function(req, res, next) {
+router.get('/contents_detail/:no', function(req, res, next) {
+	
 
-	res.render('front/cid_contents/cid_contents_detail', { });
+	var no = req.params.no;
+	var sets = {con_no : no};
+	var next = {};
+	var pre = {};
+	var contents;
+	mysql.update('update cider.cid_contents set con_viewCount = con_viewCount + 1 where con_no = :con_no', sets ,function (err, data){
+		if(err){
+			res.redirect('back');
+		}
+		mysql.select('select * from cider.cid_contents where con_no = '+ no +'', function (err, data){
+			
+			if(err){
+				res.redirect('back');
+			} 
+			var lang = data[0].con_category;
+			contents = data;
+			
+//			mysql.select('SELECT con_no, con_title FROM raonomics.rw_content WHERE con_no > '+ no +' and con_category = "'+ lang +'" LIMIT 1 UNION ALL ( SELECT con_no, con_title FROM raonomics.rw_content WHERE con_no < '+ no +' and con_category = "'+ lang +'" ORDER BY con_no DESC LIMIT 1 ) order by con_no desc' , function (err, data){
+			mysql.select('(SELECT con_no, con_title FROM cider.cid_contents WHERE con_no > '+ no +' and con_category = "'+ lang +'" LIMIT 1) UNION ( SELECT con_no, con_title FROM cider.cid_contents WHERE con_no < '+ no +' and con_category = "'+ lang +'" ORDER BY con_no DESC LIMIT 1 ) order by con_no desc' , function (err, data){				
+				if(err){
+					res.redirect('back');
+				}
+				
+				res.render('front/cid_contents/cid_contents_detail', {contents : contents, preNext : data});
+			});
+		});
+	});
+
 
 });
 
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require("./model/mysql");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('front/cid_main', { });
-	});
+	
+	var row;
+	mysql.select('select con_no, con_photo, con_title from cider.cid_contents where con_category = "1" order by con_no desc limit 0,12',
+	 function (err, data){
+		if (err) throw err;
+		console.log("data");
+		console.log(data);
+		 
+		 row = data;
+		 
+		 
+	res.render('front/cid_main', { contents : row});
+  });
+});
 
 
 router.get('/top', function(req, res, next) {
@@ -18,5 +31,7 @@ router.get('/bottom', function(req, res, next) {
 	res.render('front/bottom', { });
 
 });
+
+
 
 module.exports = router;
