999484e098961a29bff63447b8b8542a0708547a
jkhhihi <jkhhihi@naver.com>
2016-08-31 15:25:53 +0900

    * routes js finance.js 추가
    front ejs
    cid_finance, top 추가

--- routes/finance.js
+++ routes/finance.js
@@ -1,11 +1,31 @@
 var express = require('express');
 var router = express.Router();
-//var mysql = require("./model/mysql");
+var mysql = require("./model/mysql");
+
 
 router.get('/finance', function(req, res, next) {
 
 	res.render('front/cid_finance/cid_finance', { });
 
+});
+
+router.post('/finance', function(req, res, next) {
+	var consult_name = req.body.consult_name;
+	console.log(consult_name);
+	
+	
+	var posts = {consult_name : consult_name};
+	
+	//mysql.insert('insert into cider.cid_finance set = '+consult_name, function (err, data){ res.redirect('/finance'); });
+	
+	//mysql.insert('insert into cider.cid_finance SET ?', posts,  function (err, data){
+	
+	mysql.insert('insert into cider.cid_finance set consult_name = ?', consult_name,  function (err, data){
+
+	
+	res.redirect('/finance');
+	
+	});
 });
 
 
