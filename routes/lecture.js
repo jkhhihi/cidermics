6cd658d9658d56500f017b38bdaf4dfc7795e297
hayyyy <yheea403@naver.com>
2016-09-08 17:07:51 +0900

    *희아수정사항 - 콘텐츠 디테일 유투브영상 문제 해결 - 강의 메인페이지 추가 - 강의 디테일페이지 수정

--- routes/lecture.js
+++ routes/lecture.js
@@ -3,8 +3,16 @@ var router = express.Router();
 var mysql = require("./model/mysql");
 
 router.get('/lecture', function(req, res, next) {
-
-	res.render('front/cid_lecture/cid_lecture', { });
+	
+	var row;
+	mysql.select('select con_no, con_photo, con_title from cider.cid_contents order by con_viewCount desc limit 0,4', function (err, data){
+		 if (err) throw err;
+		 console.log("data");
+		 console.log(data);
+		 
+		 row = data;
+		 res.render('front/cid_lecture/cid_lecture', { contents : row});
+	});
 
 });
 
