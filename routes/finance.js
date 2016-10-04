736c5322899a3a785c1eb983aef12e7be10de767
jkhhihi <jkhhihi@naver.com>
2016-10-04 18:39:21 +0900

    *수정사항
    editor 15.jpg 추가
    
    ejs
    finance_done 추가
    our team 수정
    finance_apply
    finance_profile
    lecture_apply 수정
    
    routes/finance.js 수정

--- routes/finance.js
+++ routes/finance.js
@@ -4,6 +4,36 @@ var mysql = require("./model/mysql");
 
 var pool = require("./model/mysql");
 
+
+function getWorldTime(tzOffset) { // 24시간제
+	  var now = new Date();
+	  var tz = now.getTime() + (now.getTimezoneOffset() * 60000) + (tzOffset * 3600000);
+	  now.setTime(tz);
+
+
+	  var s =
+	    leadingZeros(now.getFullYear(), 4) + '-' +
+	    leadingZeros(now.getMonth() + 1, 2) + '-' +
+	    leadingZeros(now.getDate(), 2) + ' ' +
+
+	    leadingZeros(now.getHours(), 2) + ':' +
+	    leadingZeros(now.getMinutes(), 2) + ':' +
+	    leadingZeros(now.getSeconds(), 2);
+
+	  return s;
+}
+
+function leadingZeros(n, digits) {
+	  var zero = '';
+	  n = n.toString();
+
+	  if (n.length < digits) {
+	    for (i = 0; i < digits - n.length; i++)
+	      zero += '0';
+	  }
+	  return zero + n;
+	}
+
 router.get('/ftest', function(req, res, next) {
 	var consult_name = req.body.consult_name;
 	var row;
@@ -47,6 +77,12 @@ router.get('/ftest/checkcode', function(req, res, next) {
 router.get('/finance/profile', function(req, res, next) {
 
 	res.render('front/cid_finance/cid_finance_profile', { });
+
+});
+
+router.get('/finance/done', function(req, res, next) {
+
+	res.render('front/cid_finance/cid_finance_done', { });
 
 });
 
