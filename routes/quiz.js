b5f2eadfb2d182f8688aa7f2d49bd288c77e9720
jkhhihi <jkhhihi@naver.com>
2016-11-29 06:14:37 +0900

    *finance 수정

--- routes/quiz.js
+++ routes/quiz.js
@@ -16,9 +16,34 @@ router.get('/quiz2', function(req, res, next) {
 });
 
 router.get('/quiz3', function(req, res, next) {
+	
+	//create array
+	var questions = [];
 
-	res.render('front/cid_quiz/cid_quiz_test3', { });
-
+	//select db quiz data
+	mysql.select('select ....', function (err, data){
+	    if (err) throw err;
+	    
+	    
+	    for (var i; i<= data.length; i++) {
+	       var q = data[i].q;
+	       var options = data[i].options;
+	       var correctIndex = data[i].correctIndex;
+	       var correctResponse = data[i].correctResponse;
+	       var incorrectResponse = data[i].incorrectResponse;
+	       
+	       var obj = new Object();
+	       
+	       obj.q = q;
+	       obj.options = options;
+	       obj.correctIndex = correctIndex;
+	       obj.correctResponse = correctResponse;
+	       obj.incorrectResponse = incorrectResponse;
+	       
+	       questions.push(obj);
+	    }
+	    res.render('front/cid_quiz/cid_quiz_test3', { questions : questions});
+	});
 });
 
 module.exports = router;
